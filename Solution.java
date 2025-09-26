ef5706ef7ca69caffd3c5bb3ad9ae56fea8a1225
Nagesh Salunke <salunkenagesh14@gmail.com>
2025-09-27 01:33:17 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read N and M
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        sc.nextLine(); // consume the rest of the line
+
+        // Read grid
+        char[][] grid = new char[N][M];
+        for (int i = 0; i < N; i++) {
+            String line = sc.nextLine();
+            for (int j = 0; j < M; j++) {
+                grid[i][j] = line.charAt(j);
+            }
+        }
+
+        // Read target word
+        String word = sc.nextLine();
+
+        // TODO: implement your solution logic
+        // Example: System.out.println("YES or NO");
+    }
+}
